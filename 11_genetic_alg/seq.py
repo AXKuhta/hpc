7c9aa2d0bfddb12625d55618c52b5f6d705db2fa
AXKuhta <11133304+AXKuhta@users.noreply.github.com>
2025-12-18 20:48:58 +0800

    Add 11_genetic_alg/seq.py

---
+++ 11_genetic_alg/seq.py
@@ -0,0 +1,86 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+#
+# Target function:
+# sum over i(x.i^2 - 10cos(2 pi x.i) + 10) -> min
+#
+# N = 1000
+# x.i between -100, 100
+#
+# Population size:
+# 4096
+#
+# Fraction means island count
+#
+
+#
+# Init
+#
+x = 100*(2*np.random.rand(4096, 100)-1)
+
+def objective(x):
+	return np.sum(x*x - 10*np.cos(2*np.pi*x) + 10, -1)
+
+print("Mean fitness", np.mean(objective(x)))
+
+history = []
+
+for i in range(1000):
+	#
+	# Selection
+	#
+	a = x[:2048]
+	b = x[2048:]
+
+	u = objective(a)
+	v = objective(b)
+
+	# Winner should actually have index 0
+	# So compare u > v
+	winner = 0 + (u > v)
+
+	# For some small fraction, the unfittest actually survives
+	winner[np.random.rand(2048) > 0.8] ^= 1
+
+	# Duplicate winners
+	x = np.where(winner[:, None], b, a)
+	x = np.vstack([x, x])
+
+	#print("Mean fitness", np.mean(objective(x)))
+
+	#
+	# Cross polination (probabilistic)
+	#
+
+	a = x[:2048]
+	b = x[2048:]
+
+	# Do it once
+	swap_map = np.random.rand(2048, 100) > 0.5
+	u = np.where(swap_map, b, a)
+
+	# Do it twice
+	swap_map = np.random.rand(2048, 100) > 0.5
+	v = np.where(swap_map, b, a)
+
+	x = np.vstack([u, v])
+
+	#print("Mean fitness", np.mean(objective(x)))
+
+
+	#
+	# Mutations (probabilistic)
+	#
+
+	#
+	# Just mutate everything
+	#
+
+	x += (2*np.random.rand(4096, 100)-1)
+
+	history.append( np.mean(objective(x)) )
+
+plt.plot(history)
+plt.show()
