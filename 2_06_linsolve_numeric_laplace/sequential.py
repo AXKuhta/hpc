c7bd8cd1888a7f83584999a5b6ca9b72cf4f3481
AXKuhta <11133304+AXKuhta@users.noreply.github.com>
2026-04-19 21:28:18 +0800

    2_06_linsolve_numeric_laplace: add conjugate gradient sequential version

--- 2_06_linsolve_numeric_laplace/sequential.py
+++ 2_06_linsolve_numeric_laplace/sequential.py
@@ -93,3 +93,100 @@ def simulate_v1(ics, metal=None, tau=99):
 	#plt.imshow(x.reshape(64, 64), alpha=0.5)
 	plt.imshow(np.log10(np.abs(x.reshape(64, 64) + 0.0001)))
 	plt.show()
+
+def conjugate_gradient_lstsq(A_, b_, eps=0.01):
+	A = A_.T @ A_ # Problem needs to be least squares
+	b = A_.T @ b_
+
+	start = perf_counter()
+
+	n = b_.shape[0]
+
+	# Initial guess
+	# WARNING: Avoid using random here, it will create junk in dead parameters of ill-conditioned systems
+	x = np.zeros(n)
+	z = np.zeros_like(x)
+
+	r = b - A@x
+	d = r
+
+	delta_ = np.dot(r, r)
+	delta = delta_
+
+	log = [x]
+
+	for i in range(n):
+		q = A@d
+		alpha = delta / np.dot(d, q)
+		x = x + alpha*d
+		log.append(x)
+
+		# [optional] refresh residuals vs ground truth
+		if i % 50 == 0:
+			r = b - A@x
+		else:
+			r = r - alpha*q
+
+		delta_ = delta
+		delta = np.dot(r, r)
+		beta = delta / delta_
+		d = r + beta*d
+
+		if np.linalg.norm(log[-2] - log[-1]) < eps:
+			print("early exit: converged on step", i+1)
+			break
+
+	elapsed = perf_counter() - start
+
+	print(f"CG Elapsed {elapsed*1000:.1f}ms")
+
+	return x, elapsed
+
+def simulate_v2(ics, metal=None, tau=99):
+	h, w = ics.shape
+	n = w
+
+	assert h == w
+
+	#
+	# Use offsets: +1, -1, +n, -n
+	#
+	A = tau*np.eye(n*n, k=+1) \
+	  + tau*np.eye(n*n, k=-1) \
+	  + tau*np.eye(n*n, k=+n) \
+	  + tau*np.eye(n*n, k=-n) \
+	  - (1+4*tau)*np.eye(n*n)
+
+	# Top boundary
+	# Bottom boundary
+	A[:n, :] = 0
+	A[-n:, :] = 0
+
+	# Left boundary
+	# Right boundary
+	for i in range(n):
+		A[i*n, :] = 0
+		A[i*n + n - 1, :] = 0
+
+	A = np.float32(A)
+	b = np.float32(ics).flatten()
+
+	# Metal cutouts if metal zone specified
+	if metal is not None:
+		for i in np.argwhere(metal.flatten() == 0):
+			A[i, :] = 0
+			A[:, i] = 0
+			b[i] = 0
+
+	start = perf_counter()
+
+	x, _ = conjugate_gradient_lstsq(A, -b, eps=0.001)
+
+	elapsed = perf_counter() - start
+
+	print(f"Elapsed: {elapsed*1000:.1f}ms")
+
+	#plt.imshow(metal, alpha=0.5)
+	#plt.imshow(x.reshape(64, 64))
+	plt.imshow(np.log10(np.abs(x.reshape(64, 64) + 0.0001)))
+	plt.show()
